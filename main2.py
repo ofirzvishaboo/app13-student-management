c98a8822afa4aeb99a16ba4bde7747c7e0d2df47
Ofir <ofirfirst@gmail.com>
2023-07-31 22:09:55 +0300

    new MAIN

--- main2.py
+++ main2.py
@@ -1,35 +1,45 @@
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, \
+     QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem, QDialog, \
+     QVBoxLayout, QComboBox, QToolBar
+from PyQt6.QtGui import QAction, QIcon
 import sys
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout,\
-    QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem, QDialog, QComboBox
-from PyQt6.QtGui import QAction
 import sqlite3
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Student Management System")
+        self.setMinimumSize(800, 600)
 
         file_menu_item = self.menuBar().addMenu("&File")
         help_menu_item = self.menuBar().addMenu("&Help")
         edit_menu_item = self.menuBar().addMenu("&Edit")
 
-        edit_menu_action = QAction("Search", self)
-        edit_menu_item.addAction(edit_menu_action)
-        edit_menu_action.triggered.connect(self.my_ search)
-
-        add_student_action = QAction("Add Student", self)
+        add_student_action = QAction(QIcon("icons/add.png"), "Add Student", self)
         add_student_action.triggered.connect(self.insert)
         file_menu_item.addAction(add_student_action)
-        
+
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
         about_action.setMenuRole(QAction.MenuRole.NoRole)
 
+        search_action = QAction(QIcon("icons/search.png"), "Search", self)
+        edit_menu_item.addAction(search_action)
+        search_action.triggered.connect(self.search)
+        # Create a table with
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
         self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
+        # Create toolbar and add toolbar elements
+        toolbar = QToolBar()
+        toolbar.setMovable(True)
+        self.addToolBar(toolbar)
+        toolbar.addAction(add_student_action)
+        toolbar.addAction(search_action)
 
     def load_data(self):
         connection = sqlite3.connect("database.db")
@@ -45,47 +55,9 @@ class MainWindow(QMainWindow):
         dialog = InsertDialog()
         dialog.exec()
 
-    def my_search(self):
-        search_box = SearchBox()
-        search_box.exec()
-
-class SearchBox(QDialog):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Search Student")
-        self.setFixedWidth(300)
-        self.setFixedHeight(300)
-
-        layout = QVBoxLayout()
-        self.student_name = QLineEdit()
-        self.student_name.setPlaceholderText("Name")
-        layout.addWidget(self.student_name)
-        self.setLayout(layout)
-
-        self.table = QTableWidget()
-        self.table.setColumnCount(4)
-        self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
-        self.table.verticalHeader().setVisible(False)
-        layout.addWidget(self.table)
-
-        button = QPushButton("Search")
-        button.clicked.connect(self.name_search)
-        layout.addWidget(button)
-
-
-    def name_search(self):
-        name = self.student_name.text()
-        connection = sqlite3.connect("database.db")
-        # cursor = connection.cursor()
-        result = connection.execute("SELECT * FROM students WHERE name=?", (name,))
-        self.table.setRowCount(0)
-        for column_number, data in enumerate(result):
-            self.table.setItem(1, column_number, QTableWidgetItem(str(data)))
-        connection.close()
-        SearchBox()
-
-
-
+    def search(self):
+        dialog = SearchDialog()
+        dialog.exec()
 
 
 class InsertDialog(QDialog):
@@ -96,6 +68,7 @@ class InsertDialog(QDialog):
         self.setFixedHeight(300)
 
         layout = QVBoxLayout()
+
         # Add student name widget
         self.student_name = QLineEdit()
         self.student_name.setPlaceholderText("Name")
@@ -106,17 +79,18 @@ class InsertDialog(QDialog):
         courses = ["Biology", "Math", "Astronomy", "Physics"]
         self.course_name.addItems(courses)
         layout.addWidget(self.course_name)
-        self.setLayout(layout)
 
         # Add mobile widget
         self.mobile = QLineEdit()
-        self.mobile.setPlaceholderText("Name")
+        self.mobile.setPlaceholderText("Mobile")
         layout.addWidget(self.mobile)
 
-        # Add submit button
+        # Add a submit button
         button = QPushButton("Register")
         button.clicked.connect(self.add_student)
         layout.addWidget(button)
+
+        self.setLayout(layout)
 
     def add_student(self):
         name = self.student_name.text()
@@ -132,8 +106,44 @@ class InsertDialog(QDialog):
         main_window.load_data()
 
 
+class SearchDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        # Set window title and size
+        self.setWindowTitle("Search Student")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        # Create layout and input widget
+        layout = QVBoxLayout()
+        self.student_name = QLineEdit()
+        self.student_name.setPlaceholderText("Name")
+        layout.addWidget(self.student_name)
+
+        # Create button
+        button = QPushButton("Search")
+        button.clicked.connect(self.search)
+        layout.addWidget(button)
+
+        self.setLayout(layout)
+
+    def search(self):
+        name = self.student_name.text()
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        result = cursor.execute("SELECT * FROM students WHERE name = ?", (name,))
+        row = list(result)[0]
+        print(row)
+        items = main_window.table.findItems(name, Qt.MatchFlag.MatchFixedString)
+        for item in items:
+            print(item)
+            main_window.table.item(item.row(), 1).setSelected(True)
+
+        cursor.close()
+        connection.close()
+
+
 app = QApplication(sys.argv)
-# Call the fun action
 main_window = MainWindow()
 main_window.show()
 main_window.load_data()
